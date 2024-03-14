b31c2f791cb825cfe3413ce03c352d940237e21f
jadeleonard <lukabartolomeo9@gmail.com>
2024-03-15 02:31:39 +0800

    added

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -7,9 +7,9 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
-import { Input } from './ui/input';
-import Form from './Form';
 
+
+import * as AspectRatio from '@radix-ui/react-aspect-ratio';
 const CarouselComponent = () => {
   return (
     <div className='relative p-2'>
@@ -20,20 +20,47 @@ const CarouselComponent = () => {
           }),
         ]}
       >
-        <Form />
-       <CarouselContent className='w-full'>
-        <CarouselItem>
-         
-          <img src='https://cdn.pixabay.com/photo/2015/01/15/12/46/woman-600225_1280.jpghttps://cdn.pixabay.com/photo/2015/01/15/12/46/woman-600225_1280.jpg' className='w-full' />
-
+          
+       <CarouselContent className='w-1/2'>
+        
+        <CarouselItem className='shadow-blackA4 w-[300px] overflow-hidden rounded-md shadow-[0_2px_10px]'>
+        <AspectRatio.Root ratio={16 / 9}>
+          <img src='https://cdn.pixabay.com/photo/2015/01/15/12/46/woman-600225_1280.jpghttps://cdn.pixabay.com/photo/2015/01/15/12/46/woman-600225_1280.jpg' className='h-full w-full object-cover' />
+          </AspectRatio.Root>
         </CarouselItem>
-        <CarouselItem>
-            <img src='https://cdn.pixabay.com/photo/2017/08/01/11/48/woman-2564660_1280.jpg'  className='w-full'/>
+        <CarouselItem className='shadow-blackA4 w-[300px] overflow-hidden rounded-md shadow-[0_2px_10px]'>
+        <AspectRatio.Root ratio={16 / 9}>
+            <img src='https://cdn.pixabay.com/photo/2017/08/01/11/48/woman-2564660_1280.jpg'  className='h-full w-full object-cover'/>
+            </AspectRatio.Root>
           </CarouselItem>
-          <CarouselItem>
-             <img src='https://cdn.pixabay.com/photo/2016/03/27/19/57/cold-1284028_1280.jpg' className='w-full' />
+          <CarouselItem className='shadow-blackA4 w-[300px] overflow-hidden rounded-md shadow-[0_2px_10px]'>
+          <AspectRatio.Root ratio={16 / 9}>
+             <img src='https://cdn.pixabay.com/photo/2014/08/05/10/27/iphone-410311_1280.jpg' className='h-full w-full object-cover' />
+             </AspectRatio.Root>
           </CarouselItem>
+          <CarouselItem className='shadow-blackA4 w-[300px] overflow-hidden rounded-md shadow-[0_2px_10px]'>
+          <AspectRatio.Root ratio={16 / 9}>
+             <img src='https://cdn.pixabay.com/photo/2015/01/01/23/55/telephone-586266_1280.jpg' className='h-full w-full object-cover' />
+             </AspectRatio.Root>
+          </CarouselItem>
+          <CarouselItem className='shadow-blackA4 w-[300px] overflow-hidden rounded-md shadow-[0_2px_10px]'>
+          <AspectRatio.Root ratio={16 / 9}>
+             <img src='https://cdn.pixabay.com/photo/2016/12/10/16/57/shoes-1897708_640.jpg' className='h-full w-full object-cover' />
+             </AspectRatio.Root>
+          </CarouselItem>
+          <CarouselItem className='shadow-blackA4 w-[300px] overflow-hidden rounded-md shadow-[0_2px_10px]'>
+          <AspectRatio.Root ratio={16 / 9}>
+             <img src='https://cdn.pixabay.com/photo/2020/08/24/21/40/fashion-5515135_640.jpg' className='h-full w-full object-cover' />
+             </AspectRatio.Root>
+          </CarouselItem>
+          <CarouselItem className='shadow-blackA4 w-[300px] overflow-hidden rounded-md shadow-[0_2px_10px]'>
+          <AspectRatio.Root ratio={16 / 9}>
+             <img src='https://cdn.pixabay.com/photo/2022/07/21/20/25/converse-7336903_1280.jpg' className='h-full w-full object-cover' />
+             </AspectRatio.Root>
+          </CarouselItem>
+          
        </CarouselContent>
+          
       </Carousel>
    
     </div>
--- src/publicPages/LandingPage.jsx
+++ src/publicPages/LandingPage.jsx
@@ -19,7 +19,7 @@ const LandingPage = () => {
        <CarouselComponent />
         
 
- 
+        
     </div>
   )
 }
